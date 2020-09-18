e534e40844b0cf345e39aa78b88ff5589df78620
Tiago de Souza <tiago-desouza@outlook.com>
2020-09-18 15:44:15 -0300

    Inicialização do projeto

---
+++ src/main/java/com/tssouza/incrementalcity/IncrementalcityApplication.java
@@ -0,0 +1,13 @@
+package com.tssouza.incrementalcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncrementalcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncrementalcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tssouza/incrementalcity/IncrementalcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tssouza.incrementalcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncrementalcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
