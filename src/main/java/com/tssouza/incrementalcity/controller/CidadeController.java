53e2d013407696a001ccea79b20c9b9e30beda3b
Tiago de Souza <tiago-desouza@outlook.com>
2020-09-23 07:59:30 -0300

    Ler arquivo csv

--- src/main/java/com/tssouza/incrementalcity/controller/CidadeController.java
+++ src/main/java/com/tssouza/incrementalcity/controller/CidadeController.java
@@ -1,16 +1,35 @@
 package com.tssouza.incrementalcity.controller;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 
 @RestController
 @RequestMapping("/cidade")
 public class CidadeController {
 
 	@PostMapping("/add")
-	public String lerCaminho(@RequestBody String uri) {
-		return uri;
+	public void lerCaminho(@RequestParam String file) throws IOException, CsvException {
+//		String pathWindows = file.replace("\\", "\\\\");
+//		Path path = Paths.get(pathWindows);
+//		Reader reader = Files.newBufferedReader(Paths.get("C:\\Users\\Tiago\\Desktop\\nomes.csv"));
+		Reader reader = Files.newBufferedReader(Paths.get(file));
+		CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+
+		List<String[]> pessoas = csvReader.readAll();
+		for (String[] pessoa : pessoas) {
+			System.out.println("Name : " + pessoa[0] + " - Idade : " + pessoa[1] + " - Email : " + pessoa[2]);
+		}
 	}
 }
